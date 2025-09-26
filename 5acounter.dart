15254aa4f01c4bf1eeb7680438637c0a45ab12c5
Manasadeviakula <151750945+Manasadeviakula@users.noreply.github.com>
2025-09-26 09:54:29 +0530

    Refactor counter app to include stateless and stateful widgets

--- 5acounter.dart
+++ 5acounter.dart
@@ -1,49 +1,91 @@
 import 'package:flutter/material.dart';
 void main() {
-  runApp(SetStateDemoApp());
+  runApp(StateDemoApp());
 }
-class SetStateDemoApp extends StatelessWidget {
-@override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'setState Demo',
-      home: SetStateCounterScreen(),
-    );
-  }
-}
-class SetStateCounterScreen extends StatefulWidget {
-   @override
-   _SetStateCounterScreenState createState() =>
-   _SetStateCounterScreenState();
-}
-class _SetStateCounterScreenState extends State<SetStateCounterScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++; // update state
-    });
-  }
+class StateDemoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('setState Example'),
-      ),
-      body: Center(
-        child: Column(
+    return MaterialApp(
+      title: 'Stateful & Stateless Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Stateful & Stateless Widgets'),
+        ),
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Counter value: $_counter', style: TextStyle(fontSize: 24)),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _incrementCounter,
-              child: Text('Increment Counter'),
-            ),
+            // Stateless widget example
+            MyStatelessWidget(),
+
+
+            SizedBox(height: 40),
+
+
+            // Stateful widget example
+            MyStatefulWidget(),
           ],
         ),
       ),
     );
   }
 }
+
+
+// Stateless widget: does NOT hold state
+class MyStatelessWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'I am a Stateless Widget',
+        style: TextStyle(fontSize: 24, color: Colors.blue),
+      ),
+    );
+  }
+}
+
+
+// Stateful widget: holds state (counter in this example)
+class MyStatefulWidget extends StatefulWidget {
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _counter = 0;
+   void _incrementCounter() {
+    setState(() {
+      _counter++; // updates the state
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            'I am a Stateful Widget',
+            style: TextStyle(fontSize: 24, color: Colors.green),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Counter: $_counter',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: _incrementCounter,
+            child: Text('Increment Counter'),
+          ),
+        ],
+      ),    
+    );
+  }
+}
+
+
+
 
